fc303775e1adc34e85f1fff4467ce38858addfc1
KeiShinohara0714 <zm19022@josai.ac.jp>
2020-10-20 07:12:07 +0000

    1020

--- ex6-5.js
+++ ex6-5.js
@@ -1,10 +1,3 @@
-var who ='world';
-function sayhello(){
-alert('Hello,'+ who +'!');
-}    
-function taro(){
-  who='太郎';
-}
-function hanako(){
-  who ='花子';
-}   
+function sayhello65(){var who = 'world';alert('Hello,'+ who + '!');}  
+function taro(){var who = '太郎';alert(who + 'が来た')}
+function hanako(){var who = '花子';alert(who + 'が来た')}   
