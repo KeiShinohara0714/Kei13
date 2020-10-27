e1fbfc33396029bd34f9d828b3923e150f6787ab
KeiShinohara0714 <zm19022@josai.ac.jp>
2020-10-27 07:33:49 +0000

    1027

--- ex06-5.js
+++ ex06-5.js
@@ -6,8 +6,12 @@ function sayhello65() {
 
 function taro65() {
  who65 = '太郎';
+ alert(who65);
 }
 function hanako65() {
  who65 = '花子';
+ alert(who65);
 }
-
+function gakita(){
+    alert('が来た')
+}
