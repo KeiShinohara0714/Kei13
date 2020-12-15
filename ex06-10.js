dbd9fda849787f799a66f70ec46141afe34a5a7d
KeiShinohara0714 <zm19022@josai.ac.jp>
2020-12-15 09:43:13 +0000

    1215

--- ex06-10.js
+++ ex06-10.js
@@ -1,7 +1,8 @@
 function imgchange01() {
 document.getElementById('logo01').src =
-'./image/josai.png';    
+"./image/josai.png";    
 }
-function imgchange02
+function imgchange02(){
 document.getElementById('logo01').src =
-''
+"./image.cha.jpeg";
+}
