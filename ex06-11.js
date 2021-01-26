eab43bbcea126d492e4272aa68e7c357a8208eae
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-01-26 01:42:01 +0000

    0126

--- ex06-11.js
+++ ex06-11.js
@@ -6,8 +6,8 @@ var cnt = document.timer2.moji2.value.length;
 //文字が全部表示されている確認
 if ( cnt < 11 ) {
 //現在より1文字多く切り出して表示    
-document.timer2.moji2.value = str.substr(0,cnt+1); } else {
+document.timer2.moji2.value = str.substr(0,cnt+1);} else{
 //全て表示されたら、空文字に戻す    
-document.timer2.moji2.value = ""; }  
+document.timer2.moji2.value = "";}  
 //setTimeout()を含む関数を呼び出す  
-setTimeout("hyoji2()",1000); }
+setTimeout("hyoji()",1000) }
