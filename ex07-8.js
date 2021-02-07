9742f1944caeb1a017b13435bfb0a1599e2b5847
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-02-07 02:56:57 +0000

    0207

--- ex07-8.js
+++ ex07-8.js
@@ -1,6 +1,6 @@
 function nazo78() {
-if (document.getElementById('answer').value ='夢') 
+if (document.getElementById('answer').value = '夢') 
 {alert('あたり!');} 
-if (document.getElementById('answer').value ='eise') 
+if (document.getElementById('answer').value = 'eise') 
 {alert('はずれ!');}
 }
