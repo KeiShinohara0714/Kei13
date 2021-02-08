c3f7d690d40e5f221f04268fb19d400c9999c2a3
KeiShinohara0714 <zm19022@josai.ac.jp>
2021-02-08 21:57:46 +0000

    0209

--- ex06-9.js
+++ ex06-9.js
@@ -4,12 +4,3 @@ function txtchange1() {
 function imgchange1() {
   document.getElementById('logo').src = './image/bulbon.gif';    
 }
-function show_innerHTMLtxt1(){
-  var result1 = document.getElementById('txt1').innerHTML;
-alert(result1);    
-}
-
-function show_innerHTML(){
-  var result = document.getElementById('logo').innerHTML;
-alert(result);    
-}
